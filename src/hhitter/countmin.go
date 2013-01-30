67308508cf1da21d67c79f903afb70a462a61580
t w <distributed.badger@gmail.com>
2013-01-29 16:18:46 -0800

    add exp decay to count min sketch

--- src/hhitter/countmin.go
+++ src/hhitter/countmin.go
@@ -5,32 +5,90 @@ import (
 	"hash"
 	"hash/fnv"
 	"math"
+	"time"
 )
 
 var ErrElementNotFound = errors.New("cms: element not found")
-var MAX_INT32 int32 = int32(^uint(0) >> 1)
 var LOWER32_MASK uint64 = ^uint64(0) >> 32
+var MAX_FLOAT64 float64 = math.Inf(1)
 
-// implementation of CMS (count min sketch)
+type UpdateFn func(elem *CMElement, update *CMUpdate) *CMElement
+type ReadFn func(elem *CMElement, readTime time.Time) float64
+
+// implementation of CMS (count min sketch), optionally exponentially decayed
 type CountMin struct {
-	matrix     [][]int32
+	matrix     [][]CMElement
 	numBuckets uint32      // better to be prime
 	k          uint32      // num hashes
 	h          hash.Hash64 // hash to use, we use a + b * i, where i in (0,k], and a and b are obtained from hash h's upper 32 and lower 32 bits
+	updateFn   UpdateFn
+	readFn     ReadFn
 }
 
-func MakeCMSDirect(size uint32, numHash uint32, seed int64) *CountMin {
-	mat := make([][]int32, numHash)
-	for i, _ := range mat {
-		mat[i] = make([]int32, size)
+type CMUpdate struct {
+	Key        []byte    // key
+	Weight     float64   // weight
+	UpdateTime time.Time // time of update
+}
+
+type CMElement struct {
+	Last_update time.Time
+	Weight      float64
+}
+
+func exp_decay(decay float64, prev time.Time, cur time.Time, weight float64) float64 {
+	return math.Exp(decay*(cur.Sub(prev).Seconds())) * weight
+}
+
+func expRead_from_decay(decay float64) ReadFn {
+	return func(elem *CMElement, readTime time.Time) float64 {
+		return exp_decay(decay, elem.Last_update, readTime, elem.Weight)
 	}
-	return &CountMin{mat, size, numHash, fnv.New64()}
+}
+
+func Plain_read(elem *CMElement, readTime time.Time) float64 {
+	return elem.Weight
+}
+
+func expUpdateFn_from_decay(decay float64) UpdateFn {
+	return func(elem *CMElement, update *CMUpdate) (result *CMElement) {
+		// we don't decay if it is 0
+		if elem.Weight > 0.00000001 {
+			elem.Weight = exp_decay(decay, elem.Last_update, update.UpdateTime, elem.Weight)
+		}
+		elem.Weight += update.Weight
+		elem.Last_update = update.UpdateTime
+		return elem
+	}
+}
+
+func Plain_update(elem *CMElement, update *CMUpdate) *CMElement {
+	elem.Weight += update.Weight
+	return elem
+}
+
+func MakeCMSDirect(size uint32, numHash uint32, seed int64, updateF UpdateFn, readF ReadFn) *CountMin {
+	mat := make([][]CMElement, numHash)
+	for i, _ := range mat {
+		mat[i] = make([]CMElement, size)
+	}
+	return &CountMin{mat, size, numHash, fnv.New64(), updateF, readF}
 }
 
 func MakeCMS(eps float64, p_error float64, seed int64) *CountMin {
-	size := uint32(math.Ceil(2 / eps))
-	numHashes := uint32(math.Ceil(math.Log2(1 / p_error)))
-	return MakeCMSDirect(size, numHashes, seed)
+	size, numHashes := estimate(eps, p_error)
+	return MakeCMSDirect(size, numHashes, seed, Plain_update, Plain_read)
+}
+
+func MakeExpCMS(eps float64, p_error float64, seed int64, decay float64) *CountMin {
+	size, numHashes := estimate(eps, p_error)
+	return MakeCMSDirect(size, numHashes, seed, expUpdateFn_from_decay(decay), expRead_from_decay(decay))
+}
+
+func estimate(eps float64, p_error float64) (size uint32, numHashes uint32) {
+	size = uint32(math.Ceil(2 / eps))
+	numHashes = uint32(math.Ceil(math.Log2(1 / p_error)))
+	return
 }
 
 func (cms *CountMin) getHashParams(data []byte) (uint32, uint32) {
@@ -52,27 +110,37 @@ func (cms *CountMin) getBuckets(data []byte) []uint32 {
 }
 
 // get estimated count
-func (cms *CountMin) Count(data []byte) (min int32, err error) {
-	min = MAX_INT32
+func (cms *CountMin) Count(data []byte) (min float64, err error) {
+	return cms.CountT(data, time.Now())
+}
+
+func (cms *CountMin) CountT(data []byte, readTime time.Time) (min float64, err error) {
+	min = MAX_FLOAT64
 	for i, b := range cms.getBuckets(data) {
-		cur := cms.matrix[i][b]
+		cur := cms.readFn(&cms.matrix[i][b], readTime)
 		if min > cur {
 			min = cur
 		}
 	}
-	if min == MAX_INT32 {
+	if min == MAX_FLOAT64 {
 		return 0, ErrElementNotFound
 	}
 	return min, nil
 }
 
 // increments and returns estimated count so far
-func (cms *CountMin) Update(data []byte, cnt int32) int32 {
-	min := MAX_INT32
+func (cms *CountMin) Update(data []byte, weight float64) float64 {
+	return cms.UpdateT(data, weight, time.Now())
+}
+
+func (cms *CountMin) UpdateT(data []byte, weight float64, updateTime time.Time) float64 {
+	update := &CMUpdate{data, weight, updateTime}
+	min := MAX_FLOAT64
 	for i, b := range cms.getBuckets(data) {
-		cms.matrix[i][b] += cnt
-		if min > cms.matrix[i][b] {
-			min = cms.matrix[i][b]
+		cur := &cms.matrix[i][b]
+		cur = cms.updateFn(cur, update)
+		if min > cur.Weight {
+			min = cur.Weight
 		}
 	}
 	return min
--- src/hhitter/countmin_test.go
+++ src/hhitter/countmin_test.go
@@ -7,10 +7,13 @@ import (
 	"math"
 	"math/rand"
 	"testing"
+	"time"
 )
 
+var eps float64 = 0.0001
+
 func TestCMSSanity(t *testing.T) {
-	cms := hhitter.MakeCMSDirect(100, 100, 2.0)
+	cms := hhitter.MakeCMSDirect(100, 100, 2.0, hhitter.Plain_update, hhitter.Plain_read)
 	k1 := []byte("hello")
 	cnt, _ := cms.Count(k1)
 	if 0 != cnt {
@@ -18,11 +21,23 @@ func TestCMSSanity(t *testing.T) {
 	}
 
 	for i := 0; i < 10; i++ {
-		cms.Update(k1, 1)
+		cms.Update(k1, 1.0)
 	}
 	cnt, _ = cms.Count(k1)
-	if cnt != 10 {
+	if math.Abs(cnt-10.0) > eps {
 		t.Error("should have 10")
+	}
+}
+
+func TestCMSExpSanity(t *testing.T) {
+	now := time.Now()
+	k1 := []byte("hello")
+	cms := hhitter.MakeExpCMS(0.001, 0.0001, 5, 0.1)
+	cms.UpdateT(k1, 1.0, now)
+	cnt, _ := cms.CountT(k1, now)
+
+	if math.Abs(cnt-1.0) > eps {
+		t.Errorf("cnt should be 1.0, instead was %d", cnt)
 	}
 }
 
@@ -36,7 +51,7 @@ func TestCMSAccuracy(t *testing.T) {
 	for i, _ := range items {
 		next_msb := uint32(rand.Int31n(msb))
 		items[i] = rand.Int31n(int32(1 << next_msb))
-		cms.Update(intToBuf(items[i]), 1)
+		cms.Update(intToBuf(items[i]), float64(1))
 	}
 
 	actual := make([]int32, 1<<uint32(msb))
@@ -47,7 +62,7 @@ func TestCMSAccuracy(t *testing.T) {
 	numErrors := 0
 	for item, cnt := range actual {
 		est, _ := cms.Count(intToBuf(int32(item)))
-		diff := math.Abs(float64(est-cnt)) / float64(numItems)
+		diff := math.Abs(est-float64(cnt)) / float64(numItems)
 		if diff > 1.0001 {
 			numErrors += 1
 		}
